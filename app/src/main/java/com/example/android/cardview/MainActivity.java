dbc340966a6d137d57beedbfd554365d28dfd2ee
George Kariuki <gconsummer.gk@gmail.com>
2018-02-16 15:29:36 +0300

    Menu items are not redirecting intents

--- app/src/main/java/com/example/android/cardview/MainActivity.java
+++ app/src/main/java/com/example/android/cardview/MainActivity.java
@@ -4,8 +4,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.design.internal.NavigationMenu;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -17,7 +21,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import static com.example.android.cardview.R.id.nav_menu;
+
 public class MainActivity extends AppCompatActivity implements CardFragment.OnFragmentInteractionListener {
+    private NavigationView navigationView;
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
     private NavigationMenu mNavigationMenu;
@@ -30,11 +37,42 @@ public class MainActivity extends AppCompatActivity implements CardFragment.OnFr
         setContentView(R.layout.activity_main);
 
         android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
-        Fragment fragment = fragmentManager.findFragmentById(R.id.fragmentContainer);
+        final Fragment[] fragment = {fragmentManager.findFragmentById(R.id.fragmentContainer)};
 
-        if (fragment == null){
-            fragment = new CardFragment();
-            fragmentManager.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
+        navigationView = findViewById(nav_menu);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setChecked(true);
+                Fragment fragment1 = null;
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                int id = item.getItemId();
+                switch (id){
+                    case R.id.steven_item:
+                         Toast.makeText(getApplicationContext(),"You clicked on "+ id,
+                                Toast.LENGTH_SHORT).show();
+                         return true;
+
+                    case R.id.amethyst_item:
+                        Toast.makeText(getApplicationContext(),"You clicked on "+ id,
+                                Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.connie_item:
+                        Toast.makeText(getApplicationContext(),"You clicked on "+ id,
+                                Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.lucy_item:
+                        Toast.makeText(getApplicationContext(),"You clicked on "+ id,
+                                Toast.LENGTH_SHORT).show();
+                        return true;
+                }
+                return true;
+            }
+        });
+
+        if (fragment[0] == null){
+            fragment[0] = new CardFragment();
+            fragmentManager.beginTransaction().add(R.id.fragmentContainer, fragment[0]).commit();
         }
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
